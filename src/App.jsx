17dfa00a6bf8677e9c89518e392fddb2f7d42437
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-03-19 16:11:47 +0100

    add folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import userData from '../src/userData.json'
-import Profile from './components/Profile';
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList />
     </>
   );
 };
